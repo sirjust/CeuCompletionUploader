e1435aed28dba36ae0b55328d71d1ba6213eb500
sirjust <theoperaking@yahoo.com>
2021-04-10 19:43:36 -0400

    Remove extra variables in Idaho uploader

--- AutomateIdahoUploads/Uploader.cs
+++ AutomateIdahoUploads/Uploader.cs
@@ -29,19 +29,9 @@ namespace AutomateIdahoUploads
 
         public void InputCompletions(IEnumerable<Completion> completions)
         {
-            _driver.Url = _loginInfo.IdahoLoginUrl;
-            _driver.Manage().Window.Maximize();
-            IWebElement usernameInput = _driver.FindElement(By.Id("username"));
-            IWebElement passwordInput = _driver.FindElement(By.Id("password"));
+            LoginToWebsite();
 
-            usernameInput.SendKeys(_loginInfo.IdahoId);
-            passwordInput.SendKeys(_loginInfo.IdahoPassword);
-
-            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
-            IWebElement login = wait.Until<IWebElement>(d => d.FindElement(By.XPath("//button[@type='submit']//div[@class='btn-content']")));
-            login.Click();
-
-            IWebElement courseButton = wait.Until<IWebElement>(d => d.FindElement(By.XPath("//aside[@id='nav']//section//span[@data-bind='html: title'][contains(text(),'Courses')]")));
+            IWebElement courseButton = _wait.Until(d => d.FindElement(By.XPath("//aside[@id='nav']//section//span[@data-bind='html: title'][contains(text(),'Courses')]")));
             courseButton.Click();
 
             foreach (Completion completion in completions)
@@ -59,25 +49,23 @@ namespace AutomateIdahoUploads
 
                 try
                 {
-
-                    IWebElement courseField = wait.Until<IWebElement>(d => d.FindElement(By.CssSelector(".input-sm.form-control.input-s-sm")));
+                    IWebElement courseField = _wait.Until(d => d.FindElement(By.CssSelector(".input-sm.form-control.input-s-sm")));
                     courseField.SendKeys(courseNumber);
                     Thread.Sleep(3000);
                     courseField.SendKeys(Keys.Return);
                     Thread.Sleep(3000);
-                    IWebElement firstVisibleCourse = wait.Until<IWebElement>(d => d.FindElement(By.XPath("/html[1]/body[1]/div[1]/div[1]/section[1]/section[1]/section[1]/section[1]/section[1]/section[1]/div[1]/section[1]/section[1]/section[1]/section[1]/section[1]/div[1]/ul[1]/li[1]/small[1]/span[2]")));
-                    if(courseNumber == firstVisibleCourse.GetAttribute("innerHTML"))
+                    IWebElement firstVisibleCourse = _wait.Until(d => d.FindElement(By.XPath("/html[1]/body[1]/div[1]/div[1]/section[1]/section[1]/section[1]/section[1]/section[1]/section[1]/div[1]/section[1]/section[1]/section[1]/section[1]/section[1]/div[1]/ul[1]/li[1]/small[1]/span[2]")));
+                    if (courseNumber == firstVisibleCourse.GetAttribute("innerHTML"))
                     {
-                        IWebElement manageRoster = wait.Until<IWebElement>(d => d.FindElement(By.PartialLinkText("Manage")));
-                        manageRoster.Click();
-                        IWebElement addButton = wait.Until<IWebElement>(d => d.FindElement(By.LinkText("Add")));
-                        addButton.Click();
+                        _wait.Until(d => d.FindElement(By.PartialLinkText("Manage"))).Click();
+                        _wait.Until(d => d.FindElement(By.LinkText("Add"))).Click();
                     }
                     else
                     {
                         throw new Exception();
                     }
-                } catch(Exception ex)
+                }
+                catch (Exception ex)
                 {
                     _logger.LogException(ex, completion);
                     //then go back to the previous page
@@ -89,15 +77,15 @@ namespace AutomateIdahoUploads
                 try
                 {
                     Thread.Sleep(3000);
-                    IWebElement stateField = wait.Until<IWebElement>(d => d.FindElement(By.Id("state")));
+                    IWebElement stateField = _wait.Until(d => d.FindElement(By.Id("state")));
                     stateField.Click();
                     stateField.SendKeys(Keys.ArrowDown);
                     stateField.SendKeys(Keys.Return);
                     Thread.Sleep(3000);
 
-                    IWebElement licenseField = wait.Until<IWebElement>(d => d.FindElement(By.Id("licenseNumber")));
-                    licenseField.SendKeys(license);
-                } catch(Exception ex)
+                    _wait.Until(d => d.FindElement(By.Id("licenseNumber"))).SendKeys(license);
+                }
+                catch (Exception ex)
                 {
                     _logger.LogException(ex, completion);
                     //then go back to the previous page
@@ -109,7 +97,7 @@ namespace AutomateIdahoUploads
 
                 try
                 {
-                    IWebElement dateCompletedField = wait.Until<IWebElement>(d => d.FindElement(By.Id("dateCompleted")));
+                    IWebElement dateCompletedField = _wait.Until(d => d.FindElement(By.Id("dateCompleted")));
                     dateCompletedField.Click();
                     int dateLength = 10;
                     while (dateLength > 0)
@@ -118,29 +106,30 @@ namespace AutomateIdahoUploads
                         dateLength--;
                     }
 
-                    dateCompletedField.SendKeys(String.Format("{0:MM/dd/yyyy}", completionDate));
+                    dateCompletedField.SendKeys(string.Format("{0:MM/dd/yyyy}", completionDate));
                     dateCompletedField.SendKeys(Keys.Enter);
 
-                    IWebElement scrollField = wait.Until<IWebElement>(d => d.FindElement(By.Id("formNav")));
+                    IWebElement scrollField = _wait.Until(d => d.FindElement(By.Id("formNav")));
                     scrollField.Click();
 
                     Actions actions = new Actions(_driver);
-                    actions.SendKeys(OpenQA.Selenium.Keys.End).Build().Perform();
+                    actions.SendKeys(Keys.End).Build().Perform();
 
                     Thread.Sleep(3000);
 
-                    IWebElement saveButton = wait.Until<IWebElement>(d => d.FindElement(By.CssSelector(".btn.btn-primary.btn-s-xs.btn-sm")));
+                    IWebElement saveButton = _wait.Until(d => d.FindElement(By.CssSelector(".btn.btn-primary.btn-s-xs.btn-sm")));
                     saveButton.Click();
 
                     Thread.Sleep(3000);
 
-                    IWebElement postButton = wait.Until<IWebElement>(d => d.FindElement(By.LinkText("Post")));
+                    IWebElement postButton = _wait.Until(d => d.FindElement(By.LinkText("Post")));
                     postButton.Click();
 
                     Thread.Sleep(3000);
 
                     courseButton.Click();
-                } catch(Exception ex)
+                }
+                catch (Exception ex)
                 {
                     _logger.LogException(ex, completion);
                     //then go back to the previous page
@@ -148,7 +137,8 @@ namespace AutomateIdahoUploads
                     courseButton.Click();
                     Thread.Sleep(3000);
                     continue;
-                } finally
+                }
+                finally
                 {
                     courseButton.Click();
                     Thread.Sleep(3000);
@@ -158,8 +148,13 @@ namespace AutomateIdahoUploads
 
         public void LoginToWebsite()
         {
-            throw new NotImplementedException();
+            _driver.Url = _loginInfo.IdahoLoginUrl;
+            _driver.Manage().Window.Maximize();
+            
+            _driver.FindElement(By.Id("username")).SendKeys(_loginInfo.IdahoId);
+            _driver.FindElement(By.Id("password")).SendKeys(_loginInfo.IdahoPassword);
+
+            _wait.Until(d => d.FindElement(By.XPath("//button[@type='submit']//div[@class='btn-content']"))).Click();
         }
     }
 }
-
